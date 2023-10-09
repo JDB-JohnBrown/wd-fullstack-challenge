82dacff82c8e9700f0815f4ed16131a1afc1cad7
John Brown <jdb.johnbrown@gmail.com>
2023-10-08 21:37:24 -0400

    update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,9 @@
-cd backend
+cd frontend
+yarn install
+cd ..\backend
+python -m ensurepip --upgrade
 pip install virtualenv
-python -m virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+python -m virtualenv .env
+.\.env\Scripts\activate
+pip install -r requirements.txt
+
